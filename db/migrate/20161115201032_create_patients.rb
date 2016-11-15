b854c4831e86557781da094d90e0eef4dbda654e
Isuru <github email address>
2016-11-15 17:37:22 -0500

    finished models and db

---
+++ db/migrate/20161115201032_create_patients.rb
@@ -0,0 +1,17 @@
+class CreatePatients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :patients do |t|
+      t.integer :doctor_id
+      t.string :name
+      t.string :medical_history
+      t.string :phone_number
+      t.string :address
+      t.integer :age
+      t.string :sex
+      t.string :email
+      t.string :note
+
+      t.timestamps
+    end
+  end
+end
