5730c5e2dd5935b069375b72862812054a905ba1
Wendy Xu <guangw.xu@gmail.com>
2016-11-16 10:54:45 -0500

    fixed missing controller and other missing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :prescriptions
+  resources :drugs
+  resources :patients
+  resources :doctors
   root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
