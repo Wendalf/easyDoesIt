417d2627c0e803993bd4f2716c1113a16d4e9df3
Isuru <github email address>
2016-11-16 12:31:30 -0500

    created migration

--- app/assets/javascripts/api_calls.js
+++ app/assets/javascripts/api_calls.js
@@ -1,11 +1,63 @@
-function geo_coder(location){
-    $.ajax({
-    type: 'get',
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${location}`,
-    dataType: 'json'
-  }).done(function(data) {
-      var location = data["results"][0]["geometry"]["location"]
-      console.log(location.lat,location.lng)
-      // walgreenApi(location.lat,location.lng)
-  });
-}
+// function geo_coder(location){
+//     $.ajax({
+//     type: 'get',
+//     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${location}`,
+//     dataType: 'json'
+//   }).done(function(data) {
+//       var location = data["results"][0]["geometry"]["location"]
+//       // walgreenApi(location.lat,location.lng)
+//   });
+// }
+//
+//
+// function walgreenApi(){
+//     $.ajax({
+//       type: 'POST',
+//       xhrFields: {cors: false},
+//       url: `https://services-qa.walgreens.com/api/stores/search`,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Referer":"http://www.google.com",
+//         'Content-Type': 'application/json; charset=utf-8',
+//         "apiKey":"PRTAEgfOqBERwhGobDGBFwgUhnqXE8Aj"
+//         },
+//         dataType: 'jsonp',
+//         cache: false,
+//         jsonp: 'callback',
+//       data: {
+//         "affId":"storesapi",
+//         "apiKey":"PRTAEgfOqBERwhGobDGBFwgUhnqXE8Aj",
+//         "lat":"40.7297023",
+//         "lng":"-73.9919628",
+//         "srchOpt":"",
+//         "nxtPrev":"",
+//         "requestType":"locator",
+//         "act":"fndStore",
+//         "view":"fndStoreJSON",
+//         "devinf":"Chrome"
+//       }
+//     }).done(function(data){
+//         console.log(data)
+//     });
+// }
+//
+// var dis = {
+//   "affId":"storesapi",
+//   "apiKey":"PRTAEgfOqBERwhGobDGBFwgUhnqXE8Aj",
+//   "lat":"40.7297023",
+//   "lng":"-73.9919628",
+//   "srchOpt":"",
+//   "nxtPrev":"",
+//   "requestType":"locator",
+//   "act":"fndStore",
+//   "view":"fndStoreJSON",
+//   "devinf":"Chrome"
+// }
+//
+// function api(){
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'https://services-qa.walgreens.com/api/stores/search');
+//   xhr.withCredentials = true;
+//   xhr.setRequestHeader('Content-Type', 'application/json');
+//   xhr.send(`${dis}`);
+// }
