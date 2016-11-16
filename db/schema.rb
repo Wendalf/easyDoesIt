d995351b39d1206e47428ab0cab3814d3468f015
Isuru <github email address>
2016-11-16 16:12:43 -0500

    removed doctor

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 20161116202558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "doctors", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "drug_prescriptions", force: :cascade do |t|
     t.integer  "drug_id"
     t.integer  "prescription_id"
