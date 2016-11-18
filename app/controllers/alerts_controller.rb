d290173d0407e7c45696ab5342f4fbea0d12e8ff
Bibi Khadiza <limakhadiza@yahoo.com>
2016-11-18 17:28:26 -0500

    alert form is done with assciations. Time to demo the text messages

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,34 @@
+class AlertsController < ApplicationController
+
+
+
+  def new
+  end
+
+
+  def create
+
+    t = params["time"]
+    t.each do |time|
+      if time != " "
+       result = time.to_datetime
+       @alert = Alert.create(time: result, patient_id: params["patient_id"], drug_id: params["drug_id"])
+      end
+    end
+    redirect_to user_patient_path(@alert.patient.user, @alert.patient)
+  end
+
+
+
+  private
+
+
+
+
+
+
+
+
+
+
+end
