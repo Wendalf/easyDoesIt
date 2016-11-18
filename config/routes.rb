ae360bba6c6c483d495393a373876927bc708fbf
Wendy Xu <guangw.xu@gmail.com>
2016-11-18 14:32:11 -0500

    added name, phone, address fields in devise sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :users
-  resources :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :prescriptions
   resources :drugs
   resources :alerts
